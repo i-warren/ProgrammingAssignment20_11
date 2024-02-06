f941bb182df19dd11002190cc8d8cea5301f13a8
Isaac Warren <isaacwarren25@gmail.com>
2024-02-06 16:24:03 -0500

    Created Main structure, input and Stack

--- src/MatchSymbols.java
+++ src/MatchSymbols.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class MatchSymbols {
+import java.io.File;
+import java.util.Stack;
+
+
+public class MatchSymbols {
+    public static void main(String[] args) {
+
+        // Check for command line argument
+        if (args.length != 1) {
+            System.out.println("Usage: java MatchSymbols fileName");
+            System.exit(0);
+        }
+        File file = new File(args[0]);  // Create file obj from args
+        // if file does not exist then exit
+        if (!file.exists()) {
+            System.out.println("File: " +
+                    args[0] +
+                    "does not exist.");
+            System.exit(1);
+        }
+
+        // Create a stack
+        Stack<Character> symbols =  new Stack<>();
+
+
+        System.out.println("The file scanned " +
+                (symbols.isEmpty() ? "has" : "does not have")
+                + " matching pairs of symbols");
+    }
+
+    /** Method matches grouped symbols*/
+    private static void processSymbols(Stack<Character> stack, Character ch){
+        // if stack peek and ch match remove from stack
+
+    }
 }
