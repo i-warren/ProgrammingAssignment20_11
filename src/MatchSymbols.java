4a760c20f4c03a87916dfaa8a6881506752863da
Isaac Warren <isaacwarren25@gmail.com>
2024-02-06 17:23:11 -0500

    Finished loop for stack check. Finished processSymbols method

--- src/MatchSymbols.java
+++ src/MatchSymbols.java
@@ -1,4 +1,6 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 import java.util.Stack;
 
 
@@ -15,13 +17,32 @@ public class MatchSymbols {
         if (!file.exists()) {
             System.out.println("File: " +
                     args[0] +
-                    "does not exist.");
+                    "does not exist. ");
             System.exit(1);
         }
 
         // Create a stack
         Stack<Character> symbols =  new Stack<>();
 
+        try {
+            Scanner myReader = new Scanner(file);
+            while (myReader.hasNext()) {
+                String line = myReader.nextLine();
+                // loop through chars
+                for (int i = 0; i < line.length(); i++) {
+                    char ch = line.charAt(i);
+                    // push first match to stack
+                    if (ch == '(' || ch == '{' || ch == '[') {
+                        symbols.push(ch);
+                    } // process stack
+                    else if (ch == ')' || ch == '}' || ch == ']'){
+                        processSymbols(symbols, ch);
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
 
         System.out.println("The file scanned " +
                 (symbols.isEmpty() ? "has" : "does not have")
@@ -31,6 +52,16 @@ public class MatchSymbols {
     /** Method matches grouped symbols*/
     private static void processSymbols(Stack<Character> stack, Character ch){
         // if stack peek and ch match remove from stack
-
+        if ((stack.peek() == '(' && ch == ')') ||
+                (stack.peek() == '{' && ch == '}') ||
+                (stack.peek() == '[' && ch == ']')){
+            stack.pop();
+        } // if ch is symbol and stack peek doesn't match print and exit
+        else if ((stack.peek() != '(' && ch == ')') ||
+                (stack.peek() != '{' && ch == '}') ||
+                (stack.peek() != '[' && ch == ']')){
+            System.out.println("Java code does not have matching pairs");
+            System.exit(1);
+        }
     }
 }
